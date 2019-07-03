901f4cbdedbcd64a134cff59d9ef6bca524b3a1d
AshleyCheny <AshleyCheny@gmail.com>
2019-07-03 15:51:55 -0700

    finalize component approach

--- src/Animation/TextLoop.js
+++ src/Animation/TextLoop.js
@@ -14,91 +14,69 @@ function TextLoopAnime(props) {
             delay={(e, i) => i * 100}
             translateY={[-64, 0]}>
             <div className="blue">blue</div>
-            <div className="green">green</div>
-            <div className="red">red</div>
         </Anime>
     );
 }
 
-function TextLoopComp(props) {
+/**
+ * Use react-animation-components to implement the intro animation
+ */
+function TextLoopComp() {
     // create/update data here
-    const [translateY, updateTranslate] = useState('-100px');
-    const [opacity2, updateOpacity2] = useState({ enterOpacity: 1, exitOpacity: 0.4});
-    const [opacity3, updateOpacity3] = useState({ enterOpacity: 0.4, exitOpacity: 0.15});
-    const [opacity4, updateOpacity4] = useState({ enterOpacity: 0, exitOpacity: 0});
-    const [opacity5, updateOpacity5] = useState({ enterOpacity: 0, exitOpacity: 0});
+    // enterOpactiy: end transition opacity
+    // exitOpacity: start transition opacity
+    const initialOpacity = {
+        level1Opacity: { enterOpacity: 0, exitOpacity: 1}, // fade out animation
+        level2Opacity: { enterOpacity: 1, exitOpacity: 0.4}, // fade in to level 1 animation
+        level3Opacity: { enterOpacity: 0.4, exitOpacity: 0.15}, // fade in to leve 2 animation
+        level4Opacity: { enterOpacity: 0.15, exitOpacity: 0}, // fade in to level 3 animation
+        level5Opacity: { enterOpacity: 0, exitOpacity: 0} // hide
+    }
+
+    const [opacity, updateOpacity] = useState(initialOpacity);
     const [timer, updateTimer] = useState(0);
+    let opacity2 = {...initialOpacity.level2Opacity};
+    let opacity3 = {...initialOpacity.level3Opacity};
+    let opacity4 = {...initialOpacity.level4Opacity};
+    let opacity5 = {...initialOpacity.level5Opacity};
 
     useEffect(() => {
         console.log(timer);
-        
-        if (timer < 5) {
-            // after mount, wait for 1s to finish the animation, and then update the opacity and timer
+        // update opacity state to control the animation based on timing
+        if (timer < 4) {
+            // after mount, wait for 1s to finish the animation, and then update the opacity and timer for next move
             setTimeout(() => {
                 if (timer === 0) {
-                    // second position opacity
-                    updateOpacity3({
-                        enterOpacity: 0.4,
-                        exitOpacity: 0.15
-                    });
-                    updateOpacity4({
-                        enterOpacity: 0.15,
-                        exitOpacity: 0.4
-                    });
-                } 
+                    opacity2 = {...initialOpacity.level2Opacity};
+                    opacity3 = {...initialOpacity.level3Opacity};
+                    opacity4 = {...initialOpacity.level4Opacity}
+                }
                 if (timer === 1) {
-                    // first position opacity
-                    updateOpacity2({
-                        enterOpacity: 0,
-                        exitOpacity: 1
-                    });
-                    // first position opacity
-                    updateOpacity3({
-                        enterOpacity: 1,
-                        exitOpacity: 0.4
-                    });
-                    // second position opacity
-                    updateOpacity4({
-                        enterOpacity: 0.4,
-                        exitOpacity: 1
-                    });
-                    updateOpacity5({
-                        enterOpacity: 0.15,
-                        // exitOpacity: 0.4
-                    });
+                    opacity2 = {...initialOpacity.level1Opacity};
+                    opacity3 = {...initialOpacity.level2Opacity};
+                    opacity4 = {...initialOpacity.level3Opacity}
+                    opacity5 = {...initialOpacity.level4Opacity}
                 }
 
                 if (timer === 2) {
-                    // exit opacity setting
-                    updateOpacity3({
-                        enterOpacity: 0,
-                        exitOpacity: 1
-                    });
-                    updateOpacity4({
-                        enterOpacity: 1,
-                        // exitOpacity: 1
-                    });
-                    updateOpacity5({
-                        enterOpacity: 0.4,
-                        exitOpacity: 1
-                    });
+                    opacity3 = {...initialOpacity.level1Opacity};
+                    opacity4 = {...initialOpacity.level2Opacity}
+                    opacity5 = {...initialOpacity.level3Opacity}
                 }
                 if (timer === 3) {
-                    updateOpacity4({
-                        enterOpacity: 0,
-                        // exitOpacity: 1
-                    });
-                    updateOpacity5({
-                        enterOpacity: 1,
-                        // exitOpacity: 1
-                    });
+                    opacity4 = {...initialOpacity.level1Opacity}
+                    opacity5 = {...initialOpacity.level2Opacity}
                 }
                 if (timer === 4) {
-                    updateOpacity5({
-                        enterOpacity: 0,
-                        // exitOpacity: 1
-                    });
+                    opacity5 = {...initialOpacity.level1Opacity}
                 }
+                updateOpacity({
+                    ...initialOpacity,
+                    level2Opacity: {...opacity2},
+                    level3Opacity: {...opacity3},
+                    level4Opacity: {...opacity4},
+                    level5Opacity: {...opacity5},
+                });
                 updateTimer(timer + 1);
             }, 1000);
         }
@@ -107,59 +85,51 @@ function TextLoopComp(props) {
     // use data here
     return (
         <div className='text-loop-wrapper'>
-            {/* first one, fade out at 0 */}
+            {/* first one, delay 100ms, then fade out*/}
             <FadeTransform
                 in
                 transformProps={{
                     enterTransform: `translateY(-100px)`,
-                    // exitTransform: 'translateY(-200px)'
                 }}
-                delay={0}
-                fadeProps={{
-                    enterOpacity: 0, // end transition opacity
-                    exitOpacity: 1, // start transition opacity
-                }}>
-                <h1>First one - 1</h1>
+                delay={100}
+                fadeProps={opacity.level1Opacity}>
+                <div className='text-loop-content'>First one - 1</div>
             </FadeTransform>
-            {/* second one, fade in at 0, fade out at 1*/}
+            {/* second one, delay 500ms, then fade in to level 1, wait for 1s, fade out*/}
             <FadeTransform
                 in
                 transformProps={{
-                    enterTransform: `translateY(-${timer * 100}px)`,
-                    // exitTransform: 'translateY(-300px)'
+                    enterTransform: `translateY(-${timer * 100}px)`
                 }}
-                fadeProps={opacity2}>
-                <h1>Inspired - 2</h1>
+                fadeProps={opacity.level2Opacity}>
+                <div className='text-loop-content'>Inspired - 2</div>
             </FadeTransform>
-            {/* third one,fad in at 0, */}
+            {/* third one, delay 500ms, then fade in to level 2, wait for 1s, then fade to level1, wait for 1s, fade out */}
             <FadeTransform
                 in
                 transformProps={{
-                    enterTransform: `translateY(-${timer * 100}px)`,
-                    // exitTransform: 'translateY(-300px)'
+                    enterTransform: `translateY(-${timer * 100}px)`
                 }}
-                fadeProps={opacity3}>
-                <h1>Design - 3</h1>
+                fadeProps={opacity.level3Opacity}>
+                <div className='text-loop-content'>Design - 3</div>
             </FadeTransform>
             <FadeTransform
                     in
                     transformProps={{
-                        enterTransform: `translateY(-${timer * 100}px)`,
-                        // exitTransform: 'translateY(100px)'
+                        enterTransform: `translateY(-${timer * 100}px)`
                     }}
-                    fadeProps={opacity4}
+                    fadeProps={opacity.level4Opacity}
                 >
-                <h1>Development - 4</h1>
+                <div className='text-loop-content'>Development - 4</div>
             </FadeTransform>
             <FadeTransform
                     in
                     transformProps={{
-                        enterTransform: `translateY(-${timer * 100}px)`,
-                        // exitTransform: 'translateY(100px)'
+                        enterTransform: `translateY(-${timer * 100}px)`
                     }}
-                    fadeProps={opacity5}
+                    fadeProps={opacity.level5Opacity}
                 >
-                <h1>We are Code Particle - 5</h1>
+                <div className='text-loop-content'>We are Code Particle - 5</div>
             </FadeTransform>
         {/* <Loop in iterations={5.5}>
                 <FadeTransform
@@ -173,14 +143,14 @@ function TextLoopComp(props) {
                             exitOpacity: 0
                         }}
                     >
-                    <h1>I transition from `-100px` horizontally of my initial positon and to 0.85 opacity when `in` is `true`</h1>
+                    <div>I transition from `-100px` horizontally of my initial positon and to 0.85 opacity when `in` is `true`</div>
                 </FadeTransform>
             </Loop> */}
             {/* <Stagger in>
                 {items.map(
                     item => (
                         <Fade>
-                            <h1>Each {item} will transition in with an incrementally larger delay than the previous</h1>
+                            <div>Each {item} will transition in with an incrementally larger delay than the previous</div>
                         </Fade>
                     )
                 )}
@@ -190,10 +160,10 @@ function TextLoopComp(props) {
                 {items.map(
                     item => (
                         <Fade>
-                            <h1>
+                            <div>
                             Each {item} will increment in segments of 4.
                             First is 0, Second is 100, Third is 200, Fourth is 0, fifth is 100, and so on
-                            </h1>
+                            </div>
                         </Fade>
                     )
                 )}
@@ -209,18 +179,18 @@ function TextLoopComp(props) {
                         enterOpacity: 0.85,
                     }}
                 >
-                <h1>I transition from `-100px` horizontally of my initial positon and to 0.85 opacity when `in` is `true`</h1>
+                <div>I transition from `-100px` horizontally of my initial positon and to 0.85 opacity when `in` is `true`</div>
                 </FadeTransform>
             </Loop> */}
             {/* <Transform enterTransform="translateY(100px)" in>
-                <h1>I'm transitioning from my initial position to 100px right when `in` is `true`</h1>
+                <div>I'm transitioning from my initial position to 100px right when `in` is `true`</div>
             </Transform> */}
 
             {/* <Transform enterTransform="translateY(100px)" exitTransform="translateY(-100px)" in>
-                <h1>
+                <div>
                     I'm 100px to the left of my initial position and
                     I transition 100px right of my initial when `in` is `true`
-                </h1>
+                </div>
             </Transform> */}
         </div>
     );
