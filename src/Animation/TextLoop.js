e268f30abe2dc684e27cdab20cbee5804693c803
AshleyCheny <AshleyCheny@gmail.com>
2019-07-03 18:15:11 -0700

    refactor

--- src/Animation/TextLoop.js
+++ src/Animation/TextLoop.js
@@ -1,87 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import Anime from 'react-anime';
-import { Fade, Loop, Transform, FadeTransform, Stagger } from 'react-animation-components';
-const items = ['first', 'second', 'third', 'fourth', 'fifth'];
+import { FadeTransform } from 'react-animation-components';
 
-function TextLoopAnime(props) {
-    // create/update data here
-    const [timer, updateTimer] = useState(0);
+const items = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth'];
 
-    // use data here
-    return (
-        <Anime
-            duration={1000}
-            // delay={(el, index) => index * 240}
-            translateY='-100px'>
-            <div className="blue"/>
-        </Anime>
-    );
+const getNextRoundOpacity = (timer, opacity) => {
+    // convert array to object
+    const changedOpacity = Object.keys(initialOpacity).slice(timer, items.length + 1).reduce((prevValue, currentVaule, currentIndex) => {
+        // currentValue = 'level2Opacity' for instace -> initialOpacity.level1Opacity
+        return {
+            ...prevValue,
+            [currentVaule]: initialOpacity[`level${currentIndex + 1}Opacity`]
+        }
+    }, {});
+    
+    return {
+        ...opacity,
+        ...changedOpacity
+    }
+}
+
+// create/update data here
+// enterOpactiy: end transition opacity
+// exitOpacity: start transition opacity
+// custom opacity for each level
+const initialOpacity = {
+    level1Opacity: { enterOpacity: 0, exitOpacity: 1}, // fade out animation
+    level2Opacity: { enterOpacity: 1, exitOpacity: 0.4}, // fade in to level 1 animation
+    level3Opacity: { enterOpacity: 0.4, exitOpacity: 0.15}, // fade in to leve 2 animation
+    level4Opacity: { enterOpacity: 0.15, exitOpacity: 0}, // fade in to level 3 animation
+    level5Opacity: { enterOpacity: 0, exitOpacity: 0}, // hide
+    level6Opacity: { enterOpacity: 0, exitOpacity: 0} // hide
 }
 
 /**
  * Use react-animation-components to implement the intro animation
  */
 function TextLoopComp() {
-    // create/update data here
-    // enterOpactiy: end transition opacity
-    // exitOpacity: start transition opacity
-    // custom opacity for each level
-    const initialOpacity = {
-        level1Opacity: { enterOpacity: 0, exitOpacity: 1}, // fade out animation
-        level2Opacity: { enterOpacity: 1, exitOpacity: 0.4}, // fade in to level 1 animation
-        level3Opacity: { enterOpacity: 0.4, exitOpacity: 0.15}, // fade in to leve 2 animation
-        level4Opacity: { enterOpacity: 0.15, exitOpacity: 0}, // fade in to level 3 animation
-        level5Opacity: { enterOpacity: 0, exitOpacity: 0} // hide
-    }
-
     const [opacity, updateOpacity] = useState(initialOpacity);
     const [timer, updateTimer] = useState(0);
-    let newOpacity = {...initialOpacity};
 
+    // use setTimeout to trigger next round animation's opacity
     useEffect(() => {
         console.log(timer);
         // update opacity state to control the animation based on timing
-        // stop animation after 4s
-        if (timer < 4) {
+        // stop animation after 4s, and trigger next action
+        if (timer < items.length - 1) {
             // after mount, wait for 1s to finish the animation, and then update the opacity and timer for next move
-            setTimeout(() => {
-                if (timer === 1) {
-                    newOpacity = {
-                        ...opacity,
-                        level2Opacity:  {...initialOpacity.level1Opacity},
-                        level3Opacity: {...initialOpacity.level2Opacity},
-                        level4Opacity: {...initialOpacity.level3Opacity},
-                        level5Opacity: {...initialOpacity.level4Opacity},
-                    }
-                }
-
-                if (timer === 2) {
-                    newOpacity = {
-                        ...opacity,
-                        level3Opacity:  {...initialOpacity.level1Opacity},
-                        level4Opacity: {...initialOpacity.level2Opacity},
-                        level5Opacity: {...initialOpacity.level3Opacity},
-                    }
-                }
-                if (timer === 3) {
-                    newOpacity = {
-                        ...opacity,
-                        level4Opacity: {...initialOpacity.level1Opacity},
-                        level5Opacity: {...initialOpacity.level2Opacity},
-                    }
-                }
-                if (timer === 4) {
-                    // trigger other action for last one like close the page, open the side panel etc
-                    newOpacity = {
-                        ...opacity,
-                        level5Opacity: {...initialOpacity.level1Opacity},
-                    }
-                }
-                updateOpacity({...newOpacity});
+            const id = setTimeout(() => {
+                updateOpacity({...getNextRoundOpacity(timer, opacity)});
                 updateTimer(timer + 1);
             }, 1000);
+        } else if (timer === items.length - 1) {
+            console.log('trigger next action');
         }
-    });
+    }, [timer, items]);
 
     const renderListItem = () => {
         return items.map((item, index) => {
@@ -101,7 +74,7 @@ function TextLoopComp() {
         });
     }
 
-    // use data here
+    // use state to update the opacity
     return (
         <div className='text-loop-wrapper'>
             {renderListItem()}
@@ -171,6 +144,21 @@ function TextLoopComp() {
                 </div>
             </Transform> */}
         </div>
+    );
+}
+
+function TextLoopAnime(props) {
+    // create/update data here
+    const [timer, updateTimer] = useState(0);
+
+    // use data here
+    return (
+        <Anime
+            duration={1000}
+            // delay={(el, index) => index * 240}
+            translateY='-100px'>
+            <div className="blue"/>
+        </Anime>
     );
 }
 
